7126b161fed2da7024be64a2f14317a2c318a4a7
Michael Griffin <mike@munchdevelopment.com>
2020-10-08 21:26:33 -0500

    test

--- server/resolvers/authentication.js
+++ server/resolvers/authentication.js
@@ -41,12 +41,13 @@ module.exports = {
           user = await User.findOne({
             facebookId: id,
             email,
-          }).populate({
-            path: 'companyResponses',
-            populate: {
-              path: 'company',
-            },
           });
+          // .populate({
+          //   path: 'companyResponses',
+          //   populate: {
+          //     path: 'company',
+          //   },
+          // });
 
           console.log('user', user);
           if (!user) throw new Error(ERRORS.USER.NOT_FOUND_WITH_PROVIDED_INFO);
